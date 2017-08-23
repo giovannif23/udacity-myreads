4b80b0c96b86403944bcdb612fab6d21afe5227f
Giovanni Filippi <giovannif23@gmail.com>
2017-08-22 22:04:28 -0400

    feat: adds SearchPage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import BooksGrid from './components/molecules/BooksGrid'
+import Book from './components/molecules/Book'
+import * as BooksAPI from './BooksAPI'
 
 class SearchPage extends Component {
+  state = {
+    query: '',
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books })
+    })
+  }
+
+  updateQuery = (query) => {
+    this.setState({ query: query.trim() })
+  }
+
+  clearQuery = () => {
+    this.setState({ query: '' })
+  }
+
   render() {
+    const { query } = this.state
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+          <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
             {/*
               NOTES: The search from BooksAPI is limited to a particular set of search terms.
@@ -15,12 +40,16 @@ class SearchPage extends Component {
               However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
               you don't find a specific author or title. Every search is limited by search terms.
             */}
-            <input type="text" placeholder="Search by title or author"/>
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={query}
+              onChange={(event) => this.updateQuery(event.target.value)} />
 
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <BooksGrid books={this.state.books}></BooksGrid>
         </div>
       </div>
     )
